ec65ffc958a38670d55e6324bdcad73523c680f7
Dima Kapinus <viktoriakapinus@MacBook-Pro-Viktoria.local>
2022-10-20 12:43:00 +0300

    add File  Destructuring

---
+++ Destructuring.js
@@ -0,0 +1,12 @@
+const destructuring = {
+  name: "Dima",
+  yeras: 32,
+  gender_male: "man",
+};
+
+const { name, yeras } = destructuring;
+const { gender_male } = destructuring;
+
+console.log(name);
+console.log(yeras);
+console.log(gender_male);
