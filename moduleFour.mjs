0ee141889632a13186b0c1acd9ab5af44549a3b3
Dima Kapinus <viktoriakapinus@MacBook-Pro-Viktoria.local>
2022-10-25 14:11:23 +0300

    refactor other file

---
+++ moduleFour.mjs
@@ -0,0 +1,5 @@
+import sum from "./moduleZero.mjs";
+
+const res1 = sum(7, 6);
+
+console.log(res1);
---
+++ moduleTwo.mjs
@@ -0,0 +1,7 @@
+import { myName, sum } from "./moduleOne.mjs";
+
+myName();
+
+const res1 = sum(7, 6);
+
+console.log(res1);
